a27b31176dffb1b0bd539dc6671ca6b0e8f79384
wis <1044957202@qq.com>
2020-12-01 21:08:30 +0800

    整合日志框架

--- src/main/java/com/edu/tx/TxTest.java
+++ src/main/java/com/edu/tx/TxTest.java
@@ -1,8 +1,10 @@
 package com.edu.tx;
 
+import com.edu.tx.config.TxConfig;
 import com.edu.tx.service.UserService;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TxTest {
@@ -10,6 +12,13 @@ public class TxTest {
     @Test
     public void bank(){
         ApplicationContext context = new ClassPathXmlApplicationContext("classpath:tx2.xml");
+        UserService userService = context.getBean("userService", UserService.class);
+        userService.countMoney();
+    }
+
+    @Test
+    public void testTX(){
+        ApplicationContext context = new AnnotationConfigApplicationContext(TxConfig.class);
         UserService userService = context.getBean("userService", UserService.class);
         userService.countMoney();
     }
