d36955a299525f63c5522f5135721d00d98d8566
wis <1044957202@qq.com>
2020-12-01 21:27:17 +0800

    整合日志框架

---
+++ src/main/java/com/edu/tx/UserLog.java
@@ -0,0 +1,13 @@
+package com.edu.tx;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UserLog {
+
+    private static final Logger log = LoggerFactory.getLogger(UserLog.class);
+
+    public static void main(String[] args) {
+        log.error("test");
+    }
+}
